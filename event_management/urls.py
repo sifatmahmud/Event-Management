293d53d3b89a76b2a32b7bdecfb9d155101d371e
Sifat Mahmud <sifatmahmud5293@gmail.com>
2025-03-02 14:25:13 +0600

    model creations complete

--- event_management/urls.py
+++ event_management/urls.py
@@ -1,11 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path, include
-from events.views import dashboard
-# from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('events/', include("events.urls"))
+    # path('events/', include("events.urls"))
 ]
--- events/models.py
+++ events/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    name= models.CharField(max_length=250)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=400)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name='events')
+
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Participant(models.Model):
+    name= models.CharField(max_length=250)
+    email = models.EmailField()
+    events = models.ManyToManyField(Event, related_name="participants")
+
+    def __str__(self):
+        return self.name
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
