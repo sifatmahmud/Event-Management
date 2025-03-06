9c5dd2b72c740534848bb4d068fc54693cff6d07
Sifat Mahmud <sifatmahmud5293@gmail.com>
2025-03-06 12:19:55 +0600

    create_event, create_category, create_participant form are now more clean and managable and also successfully created message added

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from events.views import create_event, create_category, create_participant
 
 urlpatterns = [
-    path('create-event/', create_event, name='create_event'),
-    path('create-category/', create_category, name='create_category'),
-    path('create-participant/', create_participant, name='create_participant'),
+    path('create-event/', create_event, name='create-event'),
+    path('create-category/', create_category, name='create-category'),
+    path('create-participant/', create_participant, name='create-participant'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from events.forms import Event_Form, Category_Form, Participant_Form
+from django.contrib import messages
 
 
 
@@ -9,27 +10,36 @@ def create_event(request):
         create_event_form = Event_Form(request.POST)
         if create_event_form.is_valid():
             create_event_form.save()
+        
+        messages.success(request, "Event Created Successfully")
+        return redirect('create-event')
     else:
         create_event_form = Event_Form()
     
-    return render(request, 'create_event.html', {'create_event_form':create_event_form})
+    return render(request, 'forms/create_event.html', {'create_event_form':create_event_form})
 
 def create_category(request):
     if request.method == 'POST':
         create_category_form = Category_Form(request.POST)
         if create_category_form.is_valid():
             create_category_form.save()
+        
+        messages.success(request, "Category Created Successfully")
+        return redirect('create-category')
     else:
         create_category_form = Category_Form()
     
-    return render(request, 'create_category.html', {'create_category_form':create_category_form})
+    return render(request, 'forms/create_category.html', {'create_category_form':create_category_form})
 
 def create_participant(request):
     if request.method == 'POST':
         create_participant_form = Participant_Form(request.POST)
         if create_participant_form.is_valid():
             create_participant_form.save()
+        
+        messages.success(request, "Participant Created Successfully")
+        return redirect('create-participant')
     else:
         create_participant_form = Participant_Form()
     
-    return render(request, 'create_participant.html', {'create_participant_form':create_participant_form})
+    return render(request, 'forms/create_participant.html', {'create_participant_form':create_participant_form})
